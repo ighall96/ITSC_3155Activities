1209a02575e1eef769952eb328c35ca6071a1b1a
ighall96 <isaac.hall.schs@gmail.com>
2022-11-08 23:54:38 -0500

    flask app #5 - edit note function handles post and get

--- 3155Activities/flask_app/flask05.py
+++ 3155Activities/flask_app/flask05.py
@@ -58,11 +58,23 @@ def new_note():
         a_user = db.session.query(User).filter_by(email='ihall9@uncc.edu').one()
         return render_template('new.html', user=a_user)
 
-@app.route('/notes/edit/<note_id>')
+@app.route('/notes/edit/<note_id>', methods=['GET','POST'])
 def update_note(note_id):
-    a_user = db.session.query(User).filter_by(email='ihall9@uncc.edu').one()
-    my_note = db.session.query(Note).filter_by(id=note_id).one()
-    return render_template('new.html', note=my_note, user=a_user)
+    if request.method == 'POST':
+        title = request.form['title']
+        text = request.form['noteText']
+        note = db.session.query(Note).filter_by(id=note_id).one()
+        note.title = title
+        note.text = text
+        db.session.add(note)
+        db.session.commit()
+
+        return redirect(url_for('get_notes'))
+    else:
+        a_user = db.session.query(User).filter_by(email='ihall9@uncc.edu').one()
+        my_note = db.session.query(Note).filter_by(id=note_id).one()
+
+        return render_template('new.html', note=my_note, user=a_user)
        
     
 
