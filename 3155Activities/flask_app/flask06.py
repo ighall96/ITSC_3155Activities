5159ae1212f0395c9e3f8275d113c0cef68f1eef
ighall96 <isaac.hall.schs@gmail.com>
2022-11-15 22:48:39 -0500

    added registration functionality

--- 3155Activities/flask_app/flask06.py
+++ 3155Activities/flask_app/flask06.py
@@ -9,7 +9,9 @@ from flask import redirect, url_for
 from database import db
 from models import Note as Note
 from models import User as User
-
+from forms import RegisterForm
+from flask import session
+import bcrypt
 app = Flask(__name__)     # create an app
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_note_app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
@@ -19,6 +21,7 @@ with app.app_context():
 # @app.route is a decorator. It gives the function "index" special powers.
 # In this case it makes it so anyone going to "your-url/" makes this function
 # get called. What it returns is what is shown as the web page
+app.config['SECRET_KEY'] = 'SE3155'
 @app.route('/')
 @app.route('/index')
 def index():
@@ -27,10 +30,14 @@ def index():
 
 @app.route('/notes')
 def get_notes():
-    a_user = db.session.query(User).filter_by(email='ihall9@uncc.edu').one()
-    my_notes = db.session.query(Note).all()
+
+    if session.get('user'):
+        my_notes = db.session.query(Note).filter_by(user_id=session['user_id']).all()
     
-    return render_template('notes.html', notes=my_notes, user=a_user)
+        return render_template('notes.html', notes=my_notes, user=session['user'])
+    else:
+        return redirect(url_for('login'))
+    
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
@@ -84,6 +91,31 @@ def delete_note(note_id):
     db.session.commit()
 
     return redirect(url_for('get_notes'))
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    form = RegisterForm()
+
+    if request.method == 'POST' and form.validate_on_submit():
+        # salt and hash password
+        h_password = bcrypt.hashpw(
+            request.form['password'].encode('utf-8'), bcrypt.gensalt())
+        # get entered user data
+        first_name = request.form['firstname']
+        last_name = request.form['lastname']
+        # create user model
+        new_user = User(first_name, last_name, request.form['email'], h_password)
+        # add user to database and commit
+        db.session.add(new_user)
+        db.session.commit()
+        # save the user's name to the session
+        session['user'] = first_name
+        session['user_id'] = new_user.id  # access id value from user model of this newly added user
+        # show user dashboard view
+        return redirect(url_for('get_notes'))
+
+    # something went wrong - display register view
+    return render_template('register.html', form=form)
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
